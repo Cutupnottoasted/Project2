9a0d784c61a665b125e98ac97d209a591f4a1e18
cnh71 <cnh71@txstate.edu>
2023-04-03 13:04:19 -0500

    Finished description

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// project #2
+// CS4346: Artificial Intelligence
+// Due: April 17th, 2023
+// Authors: Cody Hoang, Austin Brightly, Robert Kubos
+// 
+//
+// Overview: Implement A* algorithm in C++ to a 8-puzzle game
+// Each team member must create their own heuristic function.
+// Analyze the performance of the algorithm by running the program
+// with the heuristic function discuessed in class and the team functions.
+//
+// Variables:
+//  Execution Time (ET): For each run print the execution time
+//  Nodes Generated (NG): Total number of nodes generated
+//  Nodes Expanded (NE): The total number of nodes expanded
+//  Depth of Tree (D): The depth
+//  Branching factor b*(NG/D): Effective branching factor b* = Total nodes generated / Depth of the tree
+//
+// Indvidual Work:
+//      1. Create a heuristic function (1 per individual)
+//      2. Test program with all functions and generate tables
+//      3. Analyze the result and write the conclusion
+//      4. Write the report and present to Moonis.
+//
+//
+// Initial States:
+//
+// 1) 2 | 8 | 3         2) 2 | 1 | 6
+//    1 | 6 | 4            4 | x | 8
+//    x | 7 | 5            7 | 5 | 3
+
+
+int main()
+{
+
+}
